1741516a59ec5855f06f3a4b8a32bc1acfcfa863
myles800 <50370395+myles800@users.noreply.github.com>
2019-11-16 14:52:55 +0100

    footer(sponsers)

--- EhackB/resources/views/layouts/app.blade.php
+++ EhackB/resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+@include('Partials.footer')
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -77,4 +78,5 @@
         </main>
     </div>
 </body>
+@yield('footer')
 </html>
